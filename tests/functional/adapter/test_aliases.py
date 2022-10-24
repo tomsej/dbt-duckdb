50b3449e3883a35e3f064c17dec93fe20010f774
Radek Tomšej <radek.tomsej@almamedia.com>
2022-10-24 15:49:20 +0200

    feat: include new functional tests from 1.3.0

---
+++ tests/functional/adapter/test_aliases.py
@@ -0,0 +1,19 @@
+from dbt.tests.adapter.aliases.test_aliases import (
+    BaseAliasErrors, BaseAliases, BaseSameAliasDifferentDatabases,
+    BaseSameAliasDifferentSchemas)
+
+
+class TestAliases(BaseAliases):
+    pass
+
+
+class TestAliasErrors(BaseAliasErrors):
+    pass
+
+
+class TestSameAliasDifferentSchemas(BaseSameAliasDifferentSchemas):
+    pass
+
+
+class TestSameAliasDifferentDatabases(BaseSameAliasDifferentDatabases):
+    pass
